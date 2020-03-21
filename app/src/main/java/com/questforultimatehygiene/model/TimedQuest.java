121a023241fa02f665a57e57ff359032ae9fb577
Oscheibe <contact.oliver.scheibert@gmail.com>
2020-03-21 15:47:11 +0100

    Add a rework to the timed methods functionality

--- app/src/main/java/com/questforultimatehygiene/model/TimedQuest.java
+++ app/src/main/java/com/questforultimatehygiene/model/TimedQuest.java
@@ -5,46 +5,66 @@ import java.util.TimerTask;
 
 /**
  * Subclass of Quest
- * Extends the Quest class with a Timer that is called after X seconds
+ * Extends the Quest class with a Timer that calls a function after X seconds
  */
 public class TimedQuest extends Quest {
 
-    private int time; // in seconds
+    private int reminderTime; // in seconds
+    private TimerTask reminderTask;
+    private TimerTask activityTask;
+    private boolean stopReminder = false;
     private Timer timer;
 
-    public TimedQuest(String name, String description, int experience, int time) {
+    public TimedQuest(String name, String description, int experience, int reminderTime) {
         super(name, description, experience);
-        this.time = time;
+        this.reminderTime = reminderTime;
+
         timer = new Timer();
     }
 
-    public TimedQuest(String name, String description, int experience, int time, String mediaPath) {
+    public TimedQuest(String name, String description, int experience, String mediaPath, int reminderTime) {
         super(name, description, experience, mediaPath);
-        this.time = time;
+        this.reminderTime = reminderTime;
+
         timer = new Timer();
     }
 
     /**
-     * Method that calls the run() function provided in the TimeTask after X seconds
-     * The seconds are stored in the "time" class parameter
-     * @param task with run() function that is called after X seconds
+     * Depricated
      */
-    public void startTimer(TimerTask task){
-        long delay = time * 1000; // time in seconds, delay in milliseconds
-        timer.schedule(task, delay);
+    public void startReminder(final TimerTask task){
+        final long delay = reminderTime * 1000;
+
+        reminderTask = new TimerTask() {
+            @Override
+            public void run() {
+                //if(!stopReminder)
+                    //startActivity(task, true);
+            }
+        };
+
+        // Task is called once immediately and then every X seconds
+        if(reminderTime > 0)
+            timer.schedule(reminderTask, delay);
     }
 
     /**
-     * @param time in seconds
+     * Call this method to start the task immediately and then every "reminderTime" seconds
+     * @param initialTask = method with run() component that can be executed
+     * @param taskAfter = method with run() component that can be executed
      */
-    public void setTime(int time){
-        this.time = time;
+    public void startActivity(TimerTask initialTask, TimerTask taskAfter){
+        long delay = reminderTime * 1000; // Delay milliseconds
+        timer.schedule(initialTask, 0);
+
+        if(reminderTime > 0){
+            stopReminder = false;
+            timer.schedule(taskAfter, delay);
+        }
     }
 
-    /**
-     * @return time in seconds
-     */
-    public int getTime() {
-        return time;
+    public void StopReminder() {
+        stopReminder = true;
     }
+
 }
