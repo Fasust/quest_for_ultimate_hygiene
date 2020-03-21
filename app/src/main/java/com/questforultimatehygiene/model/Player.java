b76cdc3c6c0a79694600d7a13b1fbf7c8f5f482e
Sebastian Faust <sebastian.faust@smail.th-koeln.de>
2020-03-21 12:52:41 +0100

    Add Exp Calculation to Player

--- app/src/main/java/com/questforultimatehygiene/model/Player.java
+++ app/src/main/java/com/questforultimatehygiene/model/Player.java
@@ -1,6 +1,49 @@
 package com.questforultimatehygiene.model;
 
+/**
+ * Level   XP      Difference
+ * 1       0       -
+ * 2       100     100
+ * 3       300     200
+ * 4       600     300
+ * 5       1000    400
+ * ...
+ */
 public class Player {
-    public int level;
-    public int experience;
+    static final int EXP_INCLINE = 100;
+    private final OnLevelUp onLevelUp;
+    
+    private int exp = 0;
+    private int level = 0;
+    private int expNeededTillLevelUp = 100;
+
+    public Player(OnLevelUp onLevelUp) {
+        this.onLevelUp = onLevelUp;
+    }
+
+    public void addExperience(int amount){
+        if(amount + exp > expNeededTillLevelUp){
+            levelUp((exp + amount) - expNeededTillLevelUp);
+        }else{
+            exp += amount;
+        }
+    }
+    private void levelUp(int accessExp){
+        exp = accessExp;
+        level++;
+        expNeededTillLevelUp = expNeededTillLevelUp + EXP_INCLINE;
+        onLevelUp.onLevelUp();
+    }
+
+    public int getExp() {
+        return exp;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int getExpNeededTillLevelUp() {
+        return expNeededTillLevelUp;
+    }
 }
