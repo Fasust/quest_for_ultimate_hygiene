39b9916b9462a48e9060b78cbecda48916c7649d
RWetenkamp <roman.wetenkamp@gmx.de>
2020-03-21 22:46:17 +0100

    share button moved to "Levelup popup"

--- app/src/main/java/com/questforultimatehygiene/MainActivity.java
+++ app/src/main/java/com/questforultimatehygiene/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
 
     public ViewPager viewPager;
     public ViewPagerAdapter viewPagerAdapter;
-    public ImageButton imageButton2;
+    public ImageButton imageButton;
     public TextView textView2;
 
     @Override
@@ -36,9 +36,8 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
         setContentView(R.layout.activity_main);
 
         viewPager = (ViewPager)findViewById(R.id.viewPager);
-        imageButton2 = (ImageButton)findViewById(R.id.imageButton2);
         final TextView levelCounterView =  (TextView)findViewById(R.id.level_counter_view);
-        imageButton2.setOnClickListener(this);
+
         viewPager.setPageTransformer(true, new ZoomOutPageTransformer());
         setPagerAdapter();
 
@@ -97,17 +96,20 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
         builder.setView(dialogView);
-
+        ImageButton imageButton;
+        imageButton = (ImageButton) dialogView.findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(this);
         AlertDialog alertDialog = builder.create();
         alertDialog.show();
     }
 
+    // Functionality of the sharing-button
+    // TODO: Move to class 'Popup_Levelup' as soon as this class was created; Change of the text
     @Override
     public void onClick(View view) {
-        String points = textView2.getText().toString();
         Intent sendIntent = new Intent();
         sendIntent.setAction(Intent.ACTION_SEND);
-        sendIntent.putExtra(Intent.EXTRA_TEXT, "Wow! Schon "+points+" Klopapierrollen bei 'Quest for Ultimate Hygiene'");
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "Wow! Ich bin aufgestiegen bei 'Quest for Ultimate Hygiene'");
         sendIntent.setType("text/plain");
 
         Intent shareIntent = Intent.createChooser(sendIntent, null);
